436f566e7caf5178f99056a62b42731128d9e24a
MattMistele <matthew@mistele.com>
2020-05-26 15:10:10 -0700

    Change desktop background

--- worm-core/worm-core.cpp
+++ worm-core/worm-core.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
+#include <windows.h>
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+
+	// Connect with the server, send/recieve messages
+
+	// Change desktop background
+	int return_value = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID)L"C:/Users/Matthew/Pictures/Prom/prom.JPG", SPIF_UPDATEINIFILE);
+	
+	// Encrypt file on desktop
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
